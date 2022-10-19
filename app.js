33e3e62e5caf4cd20d6d63db6918e871ce5f62a6
Khoshfekr MH <97754590+KhoshfekrMH@users.noreply.github.com>
2022-10-20 01:37:16 +0330

    send and check existence users wrote and app.js can just send user to mongoDB Atlas data base

--- app.js
+++ app.js
@@ -9,12 +9,48 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
+//region dataBase(mongoDB)
+mongoose.connect("mongodb+srv://admin-Khoshfekr:bh890Jom4JOI5lSJ@cluster0.cjnzpcu.mongodb.net/?retryWrites=true&w=majority");
+
+const userSchema = {
+    Email: String,
+    password: String
+}
+
+const User = mongoose.model("user", userSchema);
+//endregion
+
 //region page("/")
 app.get("/", function (req,res) {
     res.sendFile(__dirname + '/index.html');
 });
+
+app.post("/", function (req,res) {
+    const user1 = new User ({
+        Email: req.body.emailInput,
+        password: req.body.passwordInput
+    });
+
+    User.find({Email: req.body.emailInput},function (err, foundUser) {
+       if(!foundUser){
+           User.insertMany(user1, function (err) {
+               if(err) {
+                   console.log(err);
+               } else {
+                   console.log("User Successfully added!")
+                   res.redirect("/");
+               }
+           });
+       } else {
+           console.log("user exist")
+           res.redirect("/")
+       }
+    });
+
+});
 //endregion
 
+//region serverAddress
 let port = process.env.PORT;
 if (port == null || port === "") {
     port = 3000;
@@ -24,3 +60,4 @@ app.listen(port, function () {
         console.log("server run on port 3000");
     }
 });
+//endregion
